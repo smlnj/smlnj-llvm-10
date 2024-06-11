ee7ad99f7cc36790b2a79be5e36c52caf96e79bf
John Reppy <JohnReppy@users.noreply.github.com>
2024-06-11 09:18:19 -0500

    organizing the code so that we can use this repository as a submodule in the smlnj repository.

---
+++ smlnj/lib/mc-gen.hpp
@@ -0,0 +1,45 @@
+/// \file mc-gen.hpp
+///
+/// \copyright 2020 The Fellowship of SML/NJ (https://smlnj.org)
+/// All rights reserved.
+///
+/// \brief Wrapper class for the low-level machine-specific parts of the code generator
+///
+/// \author John Reppy
+///
+
+#ifndef _MC_GEN_HPP_
+#define _MC_GEN_HPP_
+
+#include "code-object.hpp"
+
+#include "llvm/IR/Module.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/IR/LegacyPassManager.h"
+
+class mc_gen {
+  public:
+
+    mc_gen (llvm::LLVMContext &context, const TargetInfo *target);
+
+    // per-module initialization and finalization
+    void beginModule (llvm::Module *module);
+    void endModule ();
+
+    //! run the per-function optimizations over the functions of the module
+    void optimize (llvm::Module *module);
+
+    //! dump the code to an output file
+    void dumpCode (llvm::Module *module, std::string const & stem, bool asmCode = true) const;
+
+    //! compile the code into the code buffer's object-file backing store.
+    void compile (class code_buffer *codeBuf);
+
+  private:
+    const TargetInfo *_tgtInfo;
+    std::unique_ptr<llvm::TargetMachine> _tgtMachine;
+    std::unique_ptr<llvm::legacy::FunctionPassManager> _passMngr;
+
+};
+
+#endif // !_MC_GEN_HPP_
